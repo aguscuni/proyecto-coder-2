adda3a25b2be3420d37b2d4ec6cc2b1017e47826
Agustin Cunibertti <aguscuni@gmail.com>
2022-10-07 12:07:43 -0300

    ultimos cambios

--- ProyectoCoder/AppCoder/views.py
+++ ProyectoCoder/AppCoder/views.py
@@ -3,5 +3,6 @@ from AppCoder.models import Estudiante
 
 def mostrar_inicio(request):
     estudiante = Estudiante(nombre="Exequiel", apellido="Velazquez", email="eze@gmail.com")
+    estudiante.save()
     contexto = {"estudiante_1": estudiante}
     return render(request, "AppCoder/inicio.html", contexto)
