4c0734d4f2ad5dd02f99eab91bfea4148c1404f9
Agustin Cunibertti <aguscuni@gmail.com>
2022-10-10 16:58:53 -0300

    agrego data base curso

--- ProyectoCoder/AppCoder/admin.py
+++ ProyectoCoder/AppCoder/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 
-from AppCoder.models import Estudiante
+from AppCoder.models import Estudiante, Curso
+
 # Register your models here.
+
+admin.site.register(Curso)
 
 admin.site.register(Estudiante)
 
--- ProyectoCoder/AppCoder/models.py
+++ ProyectoCoder/AppCoder/models.py
@@ -2,6 +2,15 @@ from django.db import models
 
 # Create your models here.
 
+
+class Curso(models.Model):
+    nombre = models.CharField(max_length=30)
+    camada = models.IntegerField()
+
+    def __str__(self) -> str:
+        return f"({self.camada}) {self.nombre}"
+
+
 class Estudiante(models.Model):
     nombre = models.CharField(max_length=30)
     apellido = models.CharField(max_length=30)
@@ -10,3 +19,5 @@ class Estudiante(models.Model):
 
     def __str__(self) -> str:
         return f"{self.nombre} {self.apellido}"
+
+
