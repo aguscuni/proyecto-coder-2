f85026279f1ffc303a2b578a34f430dac5eed4f8
Agustin Cunibertti <aguscuni@gmail.com>
2022-10-10 16:46:11 -0300

    new commit

--- ProyectoCoder/AppCoder/admin.py
+++ ProyectoCoder/AppCoder/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from AppCoder.models import Estudiante
 # Register your models here.
+
+admin.site.register(Estudiante)
+
--- ProyectoCoder/AppCoder/models.py
+++ ProyectoCoder/AppCoder/models.py
@@ -6,3 +6,7 @@ class Estudiante(models.Model):
     nombre = models.CharField(max_length=30)
     apellido = models.CharField(max_length=30)
     email = models.EmailField()
+
+
+    def __str__(self) -> str:
+        return f"{self.nombre} {self.apellido}"
